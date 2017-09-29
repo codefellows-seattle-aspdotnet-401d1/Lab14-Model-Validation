69dd222beea89327e322d9f585018d404865612e
Gregory Dukes <kcils360@gmail.com>
2017-09-29 09:56:24 -0700

    basic buildout finished, ready for styling

---
+++ lab14Validation/lab14Validation/Models/Students.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace lab14Validation.Models
+{
+    public class Students
+    {
+        public int ID { get; set; }
+
+        [StringLength(20, MinimumLength = 3)]
+        [Display(Name = "First Name")]
+        [Required]
+        public string FirstName { get; set; }
+
+        [StringLength(20, MinimumLength = 3)]
+        [Display(Name = "Last Name")]
+        [Required]
+        public string LastName { get; set; }
+
+        public int Grade { get; set; }
+
+        [DataType(DataType.Date)]
+        public DateTime Birthdate { get; set; }
+
+        [Display(Name = "GPA")]
+        public float GradePntAvg { get; set; }
+
+        [Display(Name = "Student ID")]
+        [Required]
+        public int StudentID { get; set; }
+    }
+}
