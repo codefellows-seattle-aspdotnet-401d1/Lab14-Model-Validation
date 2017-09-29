7d5a4caf75779a2bf99363088ea165da4abbf012
Brandon Little <brlittle86@gmail.com>
2017-09-29 11:04:06 -0700

    added validation to the Student

--- lab14-brandon/SchoolRegistry/SchoolRegistry/Models/Student.cs
+++ lab14-brandon/SchoolRegistry/SchoolRegistry/Models/Student.cs
@@ -1,16 +1,24 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
 
 namespace SchoolRegistry.Models
 {
     public class Student
     {
         public int ID { get; set; }
+
+        [StringLength(30)]
+        [Required]
         public string Name { get; set; }
+
+        [StringLength(30)]
+        [Required]
         public string Course { get; set; }
+
         public int Age { get; set; }
+
+        [DataType(DataType.Date)]
+        [Required]
         public DateTime Birthdate { get; set; }
     }
 }
