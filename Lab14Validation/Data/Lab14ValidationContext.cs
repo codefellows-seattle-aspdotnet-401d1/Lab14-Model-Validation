88236d084d39452aae6dab09981e3862e72cb23c
Austin Cohrs <acohrsdev@outlook.com>
2017-09-29 11:37:52 -0700

    austin-Lab14

---
+++ Lab14Validation/Data/Lab14ValidationContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace Lab14Validation.Models
+{
+    public class Lab14ValidationContext : DbContext
+    {
+        public Lab14ValidationContext (DbContextOptions<Lab14ValidationContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Lab14Validation.Models.StudentModel> StudentModel { get; set; }
+    }
+}
