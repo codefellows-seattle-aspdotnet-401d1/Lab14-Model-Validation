33520b242e251d55b5292977db86d5bbd2d86ab1
Thomas Reyes <thomasellison.r@gmail.com>
2017-09-29 11:26:16 -0700

    Lab finished and readme updated

---
+++ Lab14Tom/Data/Lab14TomContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace Lab14Tom.Models
+{
+    public class Lab14TomContext : DbContext
+    {
+        public Lab14TomContext (DbContextOptions<Lab14TomContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Lab14Tom.Models.Register> Register { get; set; }
+    }
+}
