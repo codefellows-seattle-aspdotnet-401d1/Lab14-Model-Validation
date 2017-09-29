891b10f81a64dd2760e5d0177207fb62212e3e9f
miyaluc <srlucas3@gmail.com>
2017-09-29 12:02:33 -0700

    added controllers, models, views, CSS

---
+++ lab14_miya/lab14_miya/Data/lab14_miyaContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace lab14_miya.Models
+{
+    public class lab14_miyaContext : DbContext
+    {
+        public lab14_miyaContext (DbContextOptions<lab14_miyaContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<lab14_miya.Models.Register> Register { get; set; }
+    }
+}
